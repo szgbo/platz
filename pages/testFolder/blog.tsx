1f7948fff49d131ff0ef85bf7eab507a54d64572
Marco Yu <>
2023-01-29 20:56:04 -0500

    path names

--- pages/testFolder/blog.tsx
+++ pages/testFolder/blog.tsx
@@ -7,21 +7,15 @@ import { initCursorChat } from 'cursor-chat'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
-const TestCursor: NextPage = () => {
-
-  useEffect(() => {
-    const cleanUp = initCursorChat('platz_cursor_chat_room')
-    return cleanUp;
-  }, []);
+const Blog: NextPage = () => {
 
   const router = useRouter();
 
   return (
-    <div id="cursor-chat-layer">
-        <input type="text" id="cursor-chat-box"></input>
+    <div>
         <div>{router.pathname}</div>
     </div>
   )
 }
 
-export default TestCursor
+export default Blog
