20885aa81076ae26a3c8189197887c1e98a24147
Dalu Liu <dalul@andrew.cmu.edu>
2023-02-05 20:12:08 -0500

    fixed cmdk styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,42 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import { Command } from 'cmdk'
 import getPosts from './api/getPosts'
+import React from 'react'
+import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 function CommandMenu({ posts: allPosts }: { posts: any }) {
+  const [open, setOpen] = React.useState(false)
+  React.useEffect(() => {
+    const downHandler = (e: any) => {
+      if (e.key === 'k' && e.metaKey) {
+        setOpen((open) => !open);
+      }
+    }
+    window.addEventListener('keydown', downHandler)
+    return () => {
+      window.removeEventListener('keydown', downHandler)
+    }
+  }, [])
   // return a command menu with all posts in pages/posts
   return (
-    <Command label="Command Menu">
-      <Command.Input />
-      <Command.List>
-        <Command.Empty>No results found.</Command.Empty>
+    <Command.Dialog open={open} onOpenChange={setOpen} label="Command Menu" className='fixed top-0 left-0 right-0 m-auto w-[45vw] mt-[20vh] bg-slate-600 p-4 rounded-lg'>
+      <Command.Input className='w-full bg-slate-600 rounded-lg border p-1 border-solid border-black mb-2' autoFocus placeholder="Search for posts..." />
+      <Command.List className='text-white'>
+        <Command.Empty >No results found.</Command.Empty>
         {/* create an item for every post */}
         {allPosts.map((post: any) => (
-          <Command.Item><a href={`/posts/${post.link}`}>{post.title}</a></Command.Item>
+          <Command.Item key={post.link} className='m-2 '><a href={`/posts/${post.link}`}>{post.title}</a></Command.Item>
         ))}
       </Command.List>
-    </Command>
+    </Command.Dialog>
   )
 }
 
 export default function Home({ allPosts }: any) {
-  console.log(allPosts)
   return (
     <>
       <Head>
@@ -31,111 +45,113 @@ export default function Home({ allPosts }: any) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <CommandMenu posts={allPosts} />
-      {/* <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
+      <div className='relative'>
+        <CommandMenu posts={allPosts} />
+        <main className={styles.main}>
+          <div className={styles.description}>
+            <p>
+              Get started by editing&nbsp;
+              <code className={styles.code}>pages/index.tsx</code>
+            </p>
+            <div>
+              <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                By{' '}
+                <Image
+                  src="/vercel.svg"
+                  alt="Vercel Logo"
+                  className={styles.vercelLogo}
+                  width={100}
+                  height={24}
+                  priority
+                />
+              </a>
+            </div>
+          </div>
+
+          <div className={styles.center}>
+            <Image
+              className={styles.logo}
+              src="/next.svg"
+              alt="Next.js Logo"
+              width={180}
+              height={37}
+              priority
+            />
+            <div className={styles.thirteen}>
+              <Image
+                src="/thirteen.svg"
+                alt="13"
+                width={40}
+                height={31}
+                priority
+              />
+            </div>
+          </div>
+
+          <div className={styles.grid}>
             <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
+              <h2 className={inter.className}>
+                Docs <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>
+                Find in-depth information about Next.js features and&nbsp;API.
+              </p>
+            </a>
+
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={inter.className}>
+                Learn <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>
+                Learn about Next.js in an interactive course with&nbsp;quizzes!
+              </p>
+            </a>
+
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={inter.className}>
+                Templates <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>
+                Discover and deploy boilerplate example Next.js&nbsp;projects.
+              </p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className={inter.className}>
+                Deploy <span>-&gt;</span>
+              </h2>
+              <p className={inter.className}>
+                Instantly deploy your Next.js site to a shareable URL
+                with&nbsp;Vercel.
+              </p>
             </a>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main> */}
+        </main>
+      </div>
     </>
   )
 }
