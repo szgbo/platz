ed3b5294155b6f8baa9af95cf97a38c3c8857ec8
Marco Wang <marcowang01@gmail.com>
2023-02-13 09:26:44 -0600

    update center component + add grid bkground

--- components/infiniteCanvas/center.tsx
+++ components/infiniteCanvas/center.tsx
@@ -2,8 +2,11 @@ import React, {useState} from 'react';
 import { type infProps, type posDict, InfDiv } from './infiniteDiv'
 
 const pos : posDict = {
-  title: [0, 0],
-  subtitle: [0, 50],
+  center: [0, 0],
+  up: [0,-50],
+  down: [0,50],
+  left: [-50,0],
+  right: [50,0],
 }
 
 const Center = (props: infProps) => {
@@ -11,7 +14,19 @@ const Center = (props: infProps) => {
   return (
     <>
       <InfDiv pos={pos.center} {...props}>
-        <h1> C </h1>
+        <h3>O</h3>
+      </InfDiv>
+      <InfDiv pos={pos.up} {...props}>
+        <h3>O</h3>
+      </InfDiv>
+      <InfDiv pos={pos.down} {...props}>
+        <h3>O</h3>
+      </InfDiv>
+      <InfDiv pos={pos.left} {...props}>
+        <h3>O</h3>
+      </InfDiv>
+      <InfDiv pos={pos.right} {...props}>
+        <h3>O</h3>
       </InfDiv>
     </>
   )
