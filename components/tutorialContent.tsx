c0de63f5391d147247e466d000a4298019b40016
Marco Wang <marcowang01@gmail.com>
2023-02-16 23:46:45 -0600

    change tutorial

--- components/tutorialContent.tsx
+++ components/tutorialContent.tsx
@@ -5,10 +5,10 @@ import styles from '../styles/Tutorial.module.css'
 
 import { type infProps, type posDict, InfDiv, InfClickDiv } from './infiniteCanvas/infiniteDiv'
 
-const offsetX = 0
+const offsetX = -375;
 const pos : posDict = {
   message: [offsetX, -50],
-  terms: [offsetX, 50],
+  terms: [offsetX, 30],
   link: [offsetX, 200],
 }
 
@@ -17,31 +17,19 @@ const TutorialContent = (props: infProps) => {
   return (
     <>
       {/* <Center {...props} /> */}
-      <InfDiv {...props} pos={pos.message}>
-        <div className={styles.message}>
-          <h2>
-            We are designing a tutorial system that empowers those 
-            without any coding background to build their own website with ease.
-          </h2>
-        </div>
+      <InfDiv {...props} pos={pos.message} align='left'>
+        <h3 className={styles.message}>
+          We&apos;re designing a tutorial system that empowers those 
+          without any coding background to build their own website with ease.
+        </h3>
       </InfDiv>
-      <InfDiv {...props} pos={pos.terms}>
-        <div className={styles.terms}>
-          <h6>
-            If you are a creative and are interested in creating a website with our features, 
-            please fill out this form and we’d love to help / better develop our tutorial system!
-          </h6>
-        </div>
+      <InfDiv {...props} pos={pos.terms} align='left'>
+        <h6 className={styles.terms}>
+          If you are a creative and are interested in creating a website with our features, <br />
+          please <a href={"https://p576j33mzl2.typeform.com/to/C78XvI3z"}>fill out this form</a> and 
+          we&apos;d love to help / better develop our tutorial system!
+        </h6>
       </InfDiv>
-      <InfClickDiv {...props} pos={pos.link}>
-        <div className={styles.link}>
-          <h3>
-            <a href={"https://p576j33mzl2.typeform.com/to/C78XvI3z"} >
-                  https://p576j33mzl2.typeform.com/to/C78XvI3z
-            </a>
-          </h3>
-        </div>
-      </InfClickDiv>
     </>
   )
 }
