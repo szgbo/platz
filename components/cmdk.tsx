17e4710477b951133459afe0b18618d112e7fc2b
szgbo <shaoboz627@gmail.com>
2023-02-17 02:23:46 -0600

    i hate tailwind

--- components/cmdk.tsx
+++ components/cmdk.tsx
@@ -29,16 +29,16 @@ export default function CommandMenu() {
         }
     }, [])
     return (
-        <div className={'fixed inset-0 justify-center items-center ' + (open ? 'backdrop-blur-sm bg-white/30 z-[100]' : '-z-[100]')}>
-            <Command.Dialog open={open} onOpenChange={setOpen} label="Command Menu" className={'fixed z-[100] inset-0 block w-[640px] max-w-[90vw] min-h-[300px] max-h-[40vh] justify-center m-auto bg-neutral-200 p-4 rounded-lg overflow-hidden'}>
-                <Command.Input autoFocus placeholder="Search for posts..." className="absolute w-full text-lg px-5 py-3 outline-none text-white bg-neutral-800 rounded-none m-0 placeholder:text-gray caret-slate-300" />
-                <Command.List className={'border-t-[1px] border-neutral-700 pt-0 mt-0 absolute bottom-0 block w-full ml-auto mr-auto overflow-y-scroll h-[85%] overscroll-contain select-none text-base text-white items-center py-2 scrollbar-thin scrollbar-thumb-neutral-300 scrollbar-track-neutral-200'}>
+        <div className={'fixed inset-0 justify-center items-center ' + (open ? 'backdrop-blur-sm bg-neutral-200/30 z-[100]' : '-z-[100]')}>
+            <Command.Dialog open={open} onOpenChange={setOpen} label="Command Menu" className={'fixed z-[100] inset-0 block w-[640px] max-w-[90vw] min-h-[300px] max-h-[40vh] justify-center m-auto bg-neutral-50 p-4 rounded-md overflow-hidden'}>
+                <Command.Input autoFocus placeholder="Search for posts..." className="absolute w-full text-lg px-4 py-2 outline-none text-neutral-600 bg-neutral-200/30 rounded-none m-0 placeholder:text-gray caret-slate-300" />
+                <Command.List className={'border-t-[1px] border-neutral-700 pt-0 mt-0 absolute bottom-0 block w-full ml-auto mr-auto overflow-y-scroll h-[85%] overscroll-contain select-none text-base text-white items-center py-2 scrollbar-none scrollbar-thumb-neutral-300 scrollbar-track-neutral-200'}>
                     <Command.Empty className='mx-[2%] px-[1%] py-4 text-center font-sans'>No results found.</Command.Empty>
                     {/* create an item for every post */}
                     {allPosts.map((post: any) => (
                         <Command.Item
                             key={post.link}
-                            className='font-sans z-[99] px-[2%] h-12 flex items-center gap-3 py-4 text-white select-none transition-all duration-150 ease-in-out relative border-l-4 border-transparent aria-selected:bg-neutral-900 aria-selected:border-cyan-500 hover:cursor-pointer'
+                            className='font-sans z-[99] px-[2%] h-12 flex items-center gap-3 py-4 text-neutral-600 select-none transition-all duration-150 ease-in-out relative border-l-4 border-transparent aria-selected:bg-neutral-200/30 aria-selected:border-neutral-200/30 hover:cursor-pointer'
                             onSelect={() => window.location.href = `/posts/${post.link}`}>
                             {post.title}
                         </Command.Item>
