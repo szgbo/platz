f051158b581b19739900f6a12485656663f9a9fa
Marco Yu <>
2023-02-19 18:25:46 -0500

    chat working, need to fix css

--- components/infiniteCanvas.tsx
+++ components/infiniteCanvas.tsx
@@ -91,7 +91,7 @@ const InfiniteCanvas = <P extends Props>(
     return (
       <div className="infinite-container">
         <div className="infinite-canvas" id={ID}>
-          <div id="cursor-chat-box"></div>
+          <input type="text" id="cursor-chat-box" />
           {/* <div className="inf-div" id="cursor-chat-layer">
             <div id="cursor-chat-box"></div>
           </div> */}
