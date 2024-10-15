6a7f2670c9e2c2a0c8f638d5f3f9cadf7c0bd692
szgbo <shaoboz627@gmail.com>
2024-10-14 17:06:37 -0700

    change

--- components/aboutContent.tsx
+++ components/aboutContent.tsx
@@ -26,7 +26,7 @@ const AboutContent = (props: infProps) => {
     {name: 'cmd k', link: 'https://cmdk.paco.me/'},
   ]
 
-  const adoptedBy = ['boboland.xyz', 'igloo.place', 'lilithyu.com']
+  const adoptedBy = ['boboland.xyz', 'igloo.place']
 
   return (
     <>
